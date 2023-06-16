c899ea8310b9aad038d991e9772e9929c9d40784
Amit Kumar <kumaramitrou@gmail.com>
2023-06-16 14:45:41 +0530

    Initial structure commit.

---
+++ devicemanager/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div><span>Logo</span><span>Header</span></div>
+    <header-component/>
+    <div>Body</div>
+    <div>Footer</div>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/Header/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
